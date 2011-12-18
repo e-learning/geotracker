2d2d9694f0ea067bfed9727a8a056ce90ddda2c8
Kirill Korgov <kirill@korgov.ru>
2011-12-19 00:16:57 +0400

    login method added

---
+++ src/ru/eltech/elerning/geotracker/core/Geo2TagParamNames.java
@@ -0,0 +1,21 @@
+package ru.eltech.elerning.geotracker.core;
+
+/**
+ * Author: Kirill Korgov (korgov@yandex-team.ru)
+ * Date: 12/18/11
+ */
+public class Geo2TagParamNames {
+    public static final String LOGIN = "login";
+    public static final String PASSWORD = "password";
+    public static final String AUTH_TOKEN = "auth_token";
+    public static final String DESCRIPTION = "description";
+    public static final String ACTIVE_RADIUS = "activeRadius";
+    public static final String RADIUS = "radius";
+    public static final String LONGITUDE = "longitude";
+    public static final String LATITUDE = "latitude";
+    public static final String TIME = "time";
+    public static final String TITLE = "title";
+    public static final String URL = "url";
+    public static final String TIME_SLOT = "timeSlot";
+    public static final String MARK_ID = "mark_id";
+}
---
+++ src/ru/eltech/elerning/geotracker/util/CollectionUtils.java
@@ -0,0 +1,28 @@
+package ru.eltech.elerning.geotracker.util;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Author: Kirill Korgov (korgov@yandex-team.ru)
+ * Date: 12/18/11
+ */
+public class CollectionUtils {
+    public static <K, V> Map<K, V> zipMap(final List<? extends K> keys, final List<? extends V> values){
+        if(keys.size() != values.size()){
+            throw new RuntimeException("List's size must be equals!");
+        }
+        final int size = keys.size();
+        final Map<K, V> out = new HashMap<K, V>();
+        for(int i = 0; i < size; ++i){
+            out.put(keys.get(i), values.get(i));
+        }
+        return out;
+    }
+
+    public static <T> List<T> list(final T... items){
+        return Arrays.asList(items);
+    }
+}
